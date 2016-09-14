27b0e330dc25315990bc8c1987c9e74527ab0145
Patrick Pelletier <code@funwithsoftware.org>
2016-09-14 15:10:49 -0700

    new android project
    created with:
    
    android create project                    \
        --activity ExampleActivity            \
        --package org.funwithsoftware.example \
        --target android-21                   \
        --path example --gradle               \
        --gradle-version 2.1.3
    
    Omitted local.properties, since that file isn't supposed to be checked in.

---
+++ src/main/java/org/funwithsoftware/example/ExampleActivity.java
@@ -0,0 +1,15 @@
+package org.funwithsoftware.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExampleActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
